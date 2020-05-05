6be463269d180f003f895884075dd8f494da009a
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-05-05 06:59:35 +0200

    adds date

--- shell.sh
+++ shell.sh
@@ -2,15 +2,23 @@
 dir=$(pwd)
 printf "Hey, welcome in learn_shell. A program that teaches about shell scripting inside the shell.\n"
 read -p "what may I call you? " username
-printf "\nokay "$username" let's get started!\n"
-printf "First, let's check where we are in time and space.\n"
-read -p " To check in which directory you are, type the command "pwd"" dir1
+echo "okay "$username" let's get started!"
+printf "\nFirst, let's check where we are in time and space.\n"
+read -p " To check in which directory you are, type the command "pwd" " dir1
 
 while [[ $dir1 != "pwd" ]]
 do
-	read -p " To check in which directory you are, type the command "pwd" " dir1
+	read -p "To check in which directory you are, type the command "pwd" " dir1
 done
-pwd
+printf "$(pwd)"
 
 printf "\nAwesome, you did well\n"
-printf "$dir \n"
+printf "So, your current directory is "$dir"\n"
+printf "Let's check now what time is it\n"
+read -p "To do so, type: "date" " dir2
+while [[ $dir2 != "date" ]]
+do
+       	read -p "Oops, it looks like you are not typing the correct command. Type "date" " dir2
+done
+printf "$(date)\n"
+printf "Wow "$userame" you learn so fast.\n"
