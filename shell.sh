bba08e157bf4778d642c71961c40a99481957181
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-05-13 09:16:21 +0200

    FINALLY

--- shell.sh
+++ shell.sh
@@ -14,17 +14,18 @@ DNA_SEQUENCES="$(pwd)${BLUE}/dna_sequences${RESET}"
 
 ## MY FUNCTIONS
 show_error() {
-    read -p " Oops, it looks like there is an error.."
+    read -p " Oops, it looks like there is an error.. "
     read -p " If this is your first time running the program, please report this problem at: benmeradi.lilyanezli@gmail.com"
     read -p " If you already ran this program once, make sure your working directory is set just as you've downloaded it the first time. "
     read -p " Or even better, I challenge you to set it back yourself using the command line! "
+	read -p " All initial files are in the backup folder. "
     
     exit 1;
 }
 
 ##Welcome and username
 printf "\t - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - -\n"
-printf "\t   Welcome to learn_shell. A program built by the digital-codon\n\t   platform, that teaches shell scripting inside the shell.\n"
+printf "\t   Welcome to Scripting In A Nutshell. An interactive program developped by  DigitalCodon  \n\t, that teaches shell scripting inside the shell.\n"
 printf "\t - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
 printf "\n"
 if [[ ! -f "$DNAFILE" ||  ! -f "$DNA2FILE" || ! -f "$DOTSECRET" || ! -d "$EMPTY_FOLDER" || -d "$DNA_SEQUENCES" ]];
@@ -438,10 +439,11 @@ do
 done
 
 printf " \t$ID${BLUE}/dna_sequences${RESET}> ${GREEN}$(wc -l dna_sequences/dna.txt 2>/dev/null)${NC}\n"
-
+read -p " Cool, you have learned so much already! "
+read -p ""
 
 ## GOODBYE
 printf "\t - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - -\n"
-printf "\t   Welcome to learn_shell. A program built by the digital-codon\n\t   platform, that teaches shell scripting inside the shell.\n"
+printf "\t   Scripting In A Nutshell. A program built by DigitalCodon\n\t, that teaches shell scripting inside the shell .\n"
 printf "\t - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
 printf "\n"
